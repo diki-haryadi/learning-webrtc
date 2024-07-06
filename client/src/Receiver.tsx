b7901ffa3836d9bf5135aefa9dc50a2d5c1065f6
Diki Haryadi <24759908+diki-haryadi@users.noreply.github.com>
2024-07-06 10:49:41 +0700

    feat: add base_url websocket

--- client/src/Receiver.tsx
+++ client/src/Receiver.tsx
@@ -16,7 +16,7 @@ export const Receiver: React.FC<any> = ({ senderStreamID }) => {
 
   const handleStartPublishing = async () => {
     websocket.current = new WebSocket(
-      'ws://103.150.93.88:7000/ws'
+      'ws://ws.haryadi.my.id/ws'
     );
     // websocket.current = new WebSocket("ws://ec2-54-248-35-65.ap-northeast-1.compute.amazonaws.com:7000/ws");
     pcSend.current = new RTCPeerConnection();
--- client/src/Sender.tsx
+++ client/src/Sender.tsx
@@ -50,7 +50,7 @@ export const Sender: React.FC<any> = ({ setSenderStreamID }) => {
     }
 
     websocket.current = new WebSocket(
-      'ws://103.150.93.88:7000/ws'
+      'ws://ws.haryadi.my.id/ws'
     );
     websocket.current.onopen = () => console.log('connection opened');
     websocket.current.onmessage = async (e) => {
